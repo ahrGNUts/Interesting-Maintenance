926c33308b45ad42d39ebed44b60b72db53d5999
Patrick <revmydev@gmail.com>
2019-03-12 15:14:48 -0700

    v1 template page content population

--- template/int-maint_template.php
+++ template/int-maint_template.php
@@ -5,12 +5,31 @@
 	@since 0.5
 	@author Patrick Strube
 */
-defined( 'ABSPATH' ) || exit; ?>
+defined( 'ABSPATH' ) || exit; 
+require( 'int-maint_template_functions.php' ); ?>
 <html>
 	<head>
 		<title>works</title>
+		<?php intmaint_echo_styles_scripts(); ?>
 	</head>
 	<body>
-		<h1>works!</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<img src="">
+					<h1 class="text-center"><?php intmaint_get_message_heading(); ?></h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<p class="text-center"><?php intmaint_get_message_body(); ?></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<iframe id="op_frame" src="<?php echo intmaint_build_sketch_url(); ?>" width="615" height="550">
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
---
+++ template/int-maint_template_functions.php
@@ -0,0 +1,42 @@
+<?php
+/*
+	Template helper functions
+	
+	@since 0.5
+	@author Patrick Strube
+*/
+defined( 'ABSPATH' ) || exit;
+
+function intmaint_build_sketch_url() {
+	$base_url = 'https://openprocessing.org/sketch/';
+	$type = get_option( '_int-maint_sketch_type' );
+	
+	switch( $type ){
+		case 'static':
+			return $base_url . get_option( '_int-maint_sketch_id' ) . '/embed';
+			break;
+		case 'pop_random':
+			//TODO
+			break;
+		case 'random':
+			//TODO
+			break;
+		default:
+			return 'https://openprocessing.org';
+			break;
+	}
+}
+
+function intmaint_get_message_heading() {
+	echo get_option( '_int-maint_message_heading' );
+}
+
+function intmaint_get_message_body() {
+	echo get_option( '_int-maint_message_body' );
+}
+
+function intmaint_echo_styles_scripts() {
+	echo '<link rel="stylesheet" href="' . INTMAINT_PLUGIN_URL . 'template/css/bootstrap.min.css">' . "\n";
+	echo '<link rel="stylesheet" href="' . INTMAINT_PLUGIN_URL . 'template/css/int-maint_styles.css">' . "\n";
+	echo '<script href="' . INTMAINT_PLUGIN_URL . 'template/js/bootstrap.min.js"></script>' . "\n";
+}
