7d58124307b6e3efb12a1b75f8e375e748c083e1
Patrick <revmydev@gmail.com>
2019-03-12 11:48:41 -0700

    version bump

--- interesting-maintenance.php
+++ interesting-maintenance.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Interesting Maintenance
 	Description: Display a more interesting maintenance or coming soon page with OpenProcessing.org sketches
-	Version: 0.4
+	Version: 0.5
 	Author: Patrick Strube
 	Text Domain: int-maint
 	License: GPLv2
